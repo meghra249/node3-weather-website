bc187b83cfffd0fab481ea25ad925d82d86d74b5
meghraj ghimire <Meghraj>
2022-02-03 13:09:23 +0400

    Temperature Change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,6 +12,8 @@ const forecast = (latitude,longitude,callback)=>{
             callback(undefined,"Current temperature is:"+body.current.temperature 
             +" Feels liks is:"+body.current.feelslike+" . Description:"+body.current.weather_descriptions)
             
+            callback(undefined,"It is currently:"+body.current.temperature +" degrees out."
+            +" It feels like :"+body.current.feelslike+" . Weather is :"+body.current.weather_descriptions)
         }
     })
 }
